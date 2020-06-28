2e65d46f11923aa585f33ba54057ad75f05a4f4e
Yves Baumes <ybaumes@gmail.com>
2020-06-28 11:07:50 +0200

    Refactor processConn() function.

--- ftp.go
+++ ftp.go
@@ -15,16 +15,21 @@ func main() {
 		if err != nil {
 			log.Fatal("Accept: ", err)
 		}
-		log.Println("remote addr = ", c.RemoteAddr())
 
-		c.Write([]byte("220 Yves FTP Ready\r\n"))
-
-		b := make([]byte, 40)
-		n, err := c.Read(b)
-		if err != nil {
-			log.Fatal("Read: ", err)
-		}
-
-		log.Printf("b:=%q[%v]", string(b[:n-2]), n)
+		go procesConn(c)
 	}
+}
+
+func procesConn(c net.Conn) {
+	log.Println("remote addr = ", c.RemoteAddr())
+
+	c.Write([]byte("220 Yves FTP Ready\r\n"))
+
+	b := make([]byte, 40)
+	n, err := c.Read(b)
+	if err != nil {
+		log.Fatal("Read: ", err)
+	}
+
+	log.Printf("b:=%q[%v]", string(b[:n-2]), n)
 }
