e5cfabb3d46582e7314d3e3bcb886cbead716c90
Yves Baumes <ybaumes@gmail.com>
2020-06-28 11:04:02 +0200

    Implement first msgs.

--- ftp.go
+++ ftp.go
@@ -16,5 +16,15 @@ func main() {
 			log.Fatal("Accept: ", err)
 		}
 		log.Println("remote addr = ", c.RemoteAddr())
+
+		c.Write([]byte("220 Yves FTP Ready\r\n"))
+
+		b := make([]byte, 40)
+		n, err := c.Read(b)
+		if err != nil {
+			log.Fatal("Read: ", err)
+		}
+
+		log.Printf("b:=%q[%v]", string(b[:n-2]), n)
 	}
 }
